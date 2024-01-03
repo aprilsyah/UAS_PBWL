cc38dd002a1ef7b650f576d435f5b66372e1070d
aprilsyah <151800483+aprilsyah@users.noreply.github.com>
2024-01-03 16:28:52 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
     <header>
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('admin.dashboard.index') }}">Laravel</a>
+                <a class="navbar-brand" href="{{ route('admin.dashboard.index') }}">Laundry</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
